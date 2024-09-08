a5288d3b11c3028f2d3ad8d26965955f494fd44c
JunpeiAbe <uihrsw0228@gmail.com>
2024-09-08 15:40:06 +0900

    Initial Commit

---
+++ SwiftUILearn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUILearn
+//
+//  Created by j on 2024/09/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUILearn/SwiftUILearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearnApp.swift
+//  SwiftUILearn
+//
+//  Created by j on 2024/09/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
